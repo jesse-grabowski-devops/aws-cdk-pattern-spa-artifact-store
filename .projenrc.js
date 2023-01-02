afa9abad08afb491e669e38eddbec6e48c4bf5a9
Jesse Grabowski <github@jessegrabowski.com>
2023-01-02 18:48:18 -0500

    feat: update projen config

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,27 @@
-const { awscdk } = require('projen');
+const { awscdk, github } = require('projen');
+const { NpmAccess } = require('projen/lib/javascript');
+
+const cdkVersion = '2.58.1';
+
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Jesse Grabowski',
-  authorAddress: 'github@jessegrabowski.com',
-  cdkVersion: '2.1.0',
+  authorAddress: 'npm@jessegrabowski.com',
+  cdkVersion,
   defaultReleaseBranch: 'main',
-  name: 'aws-cdk-pattern-spa-artifact-store',
+  name: '@npm-jessegrabowski/aws-cdk-pattern-spa-artifact-store',
   repositoryUrl: 'git@github.com:jesse-grabowski-devops/aws-cdk-pattern-spa-artifact-store.git',
-
+  description: 'A CDK construct for deploying an artifact store for static SPA files to AWS.',
+  npmAccess: NpmAccess.PUBLIC,
+  gitignore: [
+    '.idea',
+  ],
+  githubOptions: {
+    projenCredentials: github.GithubCredentials.fromApp({}),
+  },
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
 project.synth();
